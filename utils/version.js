4385f42dc57294f30fa86b5283c76691d30e7ba7
chasewindyoungs <chasewindyoungs@163.com>
2023-05-05 14:58:37 +0800

    feat: 新增版本控制，可以自定义版本参数

---
+++ utils/version.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const fs = require("fs");
+const chalk = require("chalk");
+const { getPackageInfo } = require("./file.js");
+let packageData = JSON.parse(getPackageInfo());
+let version = packageData.version;
+function main() {
+  const args = process.argv.slice(2);
+
+  function changeNum(argNum, idx) {
+    if (parseInt(version.split(".")[idx]) >= args[1]) {
+      throw new Error("version number must bigger than now!");
+      return;
+    } else return argNum || parseInt(version.split(".")[idx]) + 1;
+  }
+
+  if (args.length > 0) {
+    if (args[0] === "major") {
+      // 重大更新版本
+      version = version.split(".").fill(changeNum(args[1], 0), 0, 1).join(".");
+    } else if (args[0] === "minor") {
+      // 主要更新版本
+      version = version.split(".").fill(changeNum(args[1], 1), 1, 2).join(".");
+    } else {
+      // 补丁更新版本
+      version = version.split(".").fill(changeNum(args[1], 2), 2, 3).join(".");
+    }
+  } else {
+    // 补丁更新版本
+    version = version
+      .split(".")
+      .fill(parseInt(changeNum(null, 2)), 2, 3)
+      .join(".");
+  }
+  packageData.version = version;
+  packageData = JSON.stringify(packageData, null, 2);
+  fs.writeFileSync(path.join(process.cwd(), "./package.json"), packageData);
+  fs.writeFile(path.join(process.cwd(), "./package.json"), packageData, (err, file) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(chalk.green("Update version success!"));
+      ;
+    }
+  });
+}
+try {
+  main();
+} catch (error) {
+  console.log(chalk.red(error));
+  console.error(error);
+  process.exit(1);
+}
