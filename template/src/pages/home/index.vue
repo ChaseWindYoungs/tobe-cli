f1a33b54e52b33b25f67fd76fccbba0868c9fbcf
chasewindyoungs <chasewindyoungs@163.com>
2023-07-25 11:12:16 +0800

    feat: update home page

--- template/src/pages/home/index.vue
+++ template/src/pages/home/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <p class="content">welcome to <%= name %> project, it created by tobe-cli</p>
+  <div class="welcome">
+    <img :src="srcImg" class="logo vue" alt="Vue logo" />
+    
+    <div class="content">
+      <p >Welcome to</p>
+      <h3><%= name %> project</h3>
+      <p>It created by <a :href="href" target="_blank">tobe-cli</a></p>
+    </div>
+  </div>
 </template>
+<script setup>
+import { getAssetsFile } from '@/utils/index.js'
+import { ref } from 'vue';
+const srcImg = getAssetsFile('images/vue.svg');
+const href = ref('https://www.npmjs.com/package/tobe-cli')
+</script>
 <style lang="scss" scoped>
-.content{
-  padding: 20px;
-  color: $primary;
+.welcome {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 200px;
+  .logo {
+    width: 300px;
+    margin-right: 20px;
+  }
+  .content {
+    padding: 20px;
+    h3 {
+      font-size: 40px;
+      margin: 20px 0;
+      color: $primary;
+    }
+  }
 }
 </style>
