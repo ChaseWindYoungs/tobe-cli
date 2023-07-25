57cf55a0fe6975a515d30024c35a4782e94c4c1e
chasewindyoungs <chasewindyoungs@163.com>
2023-07-25 10:39:02 +0800

    feat: Element-Plus update version to v2.3.8

--- template/src/App.vue
+++ template/src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 import { changeThemeValue } from '@/utils';
 import { onMounted } from 'vue';
 
